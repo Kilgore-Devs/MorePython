a4001839c36da447fbdd8edd47ff19167c63c2a8
Kilgore_Devs <Dr.Dev.Kilgore@gmail.com>
2023-10-13 23:34:56 -0500

    codewars stuff

---
+++ Basics/level 8/Number Translation.py
@@ -0,0 +1,11 @@
+def funky(r, n):
+    rang = range(r)
+
+
+
+print(translate(3, 0))#, "-1 ---> 9")
+print(translate(3, 2))#, "+5 or -5 ---> 7")
+print(translate(3, 4))#, "+1 ---> 5")
+print(translate(9, 100))#, "100 is not in the range [0:81]")
+print(translate(10, 50))#, "+0 ---> 50")
+print(translate(3, 7))#, "+5 or -5 ---> 2")
